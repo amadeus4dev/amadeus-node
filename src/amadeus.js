44547ac4c4e2ac0be8220920ad72df9c47cf0fec
Cristiano Betta <cristiano@betta.io>
2018-01-31 19:41:41 +0100

    Implement client initialization

--- src/amadeus.js
+++ src/amadeus.js
@@ -5,7 +5,18 @@ import Client from './amadeus/client';
  *
  * @param {Object} options a list of options
  * @param {string} options.clientId the `client_id` used to authenticate the API
- * @param {string} options.clientSecret the `client_secret` used to authenticate the API
+ * @param {string} options.clientSecret the `client_secret` used to authenticate
+ *  the API
+ * @param {Object} [options.logger=console] an optional `console` compatible logger that
+ *  accepts `log`, `error` and `debug` calls. Defaults to a plain console.
+ * @param {string} [options.hostname='test'] the name of the host (`test` or
+ *   `production`) to make an API call against.
+ * @param {string} [options.customAppId=null] a custom App ID to be passed in the User
+ *  Agent to the server.
+ * @param {string} [options.customAppVersion=null] a custom App version to be passed
+ *  in the User Agent to the server.
+ * @param {Object} [options.http=https] an optional Node HTTPS compatible client.
+ *  Defaults to the standard Node HTTPS client.
  */
 class Amadeus {
   constructor(options = {}) {
--- src/amadeus/client/validator.js
+++ src/amadeus/client/validator.js
@@ -9,7 +9,7 @@ class Validator {
    * @param {string} key the key to find the entry
    * @param {Object} options the arguments that were passed to the {@link Amadeus} object
    * @throws {ArgumentError} when no entry can be found
-   * @memberof Validator
+   * @returns {Object} The object found by the key in the options.
    *
    */
   initRequired(key, options) {
@@ -26,13 +26,33 @@ class Validator {
    * @param {string} key the key to find the entry
    * @param {Object} options the arguments that were passed to the {@link Amadeus} object
    * @param {Object} fallback an optional default value to fall back on
-   * @memberof Validator
+   * @returns {Object} The object found by the key in the options, or the fallback value.
    */
   initOptional(key, options, fallback = null) {
     return options[key] ||
           options[key.to_s] ||
-          process.env[`AMADEUS_${key.upcase}`] ||
+          process.env[`AMADEUS_${key.toUpperCase()}`] ||
           fallback;
+  }
+
+  /**
+  * Checks a list of options for unrecognized keys and warns the user.
+  * This is mainly used to provide a nice experience when users make a typo
+  * in their arguments.
+  #
+  * @param {Object} options the arguments were passed to the {@see Amadeus}
+  *  constructor
+  * @param {Object} logger the logger used to send warnings to
+  * @param {Array} recognizedOptions a whitelist of recognized options
+  * @return {null}
+  */
+  warnOnUnrecognizedOptions(options, logger, recognizedOptions) {
+    Object.keys(options).forEach((key) => {
+      if (!recognizedOptions.includes(key)) {
+        logger.warn('amadeus/client/validator.js', `Unrecognized option: ${key}`);
+      }
+    });
+    return null;
   }
 }
 
