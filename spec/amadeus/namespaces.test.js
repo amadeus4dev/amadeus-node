bdb9b5a55acf0f6a1a79823349b9f6f5bcd8a1b4
Alvaro Navarro <alvaro.navarro@amadeus.com>
2019-07-11 16:51:31 +0200

    Add Flight Choice Prediction API

--- spec/amadeus/namespaces.test.js
+++ spec/amadeus/namespaces.test.js
@@ -40,6 +40,7 @@ describe('Namespaces', () => {
       expect(amadeus.shopping.flightDates).toBeDefined();
       expect(amadeus.shopping.flightDestinations).toBeDefined();
       expect(amadeus.shopping.flightOffers).toBeDefined();
+      expect(amadeus.shopping.flightOffers.prediction).toBeDefined();
 
       expect(amadeus.shopping.hotelOffers).toBeDefined();
       expect(amadeus.shopping.hotelOffersByHotel).toBeDefined();
@@ -68,6 +69,10 @@ describe('Namespaces', () => {
       expect(amadeus.shopping.hotelOffers.get).toBeDefined();
       expect(amadeus.shopping.hotelOffersByHotel.get).toBeDefined();
       expect(amadeus.shopping.hotelOffer('XXX').get).toBeDefined();
+    });
+
+    it('should define all expected .post methods', () => {
+      expect(amadeus.shopping.flightOffers.prediction.post).toBeDefined();
     });
 
     it('.amadeus.referenceData.urls.checkinLinks.get', () => {
@@ -173,6 +178,13 @@ describe('Namespaces', () => {
       amadeus.shopping.flightOffers.get();
       expect(amadeus.client.get)
         .toHaveBeenCalledWith('/v1/shopping/flight-offers', {});
+    });
+
+    it('.amadeus.shopping.flightOffers.prediction.post', () => {
+      amadeus.client.post = jest.fn();
+      amadeus.shopping.flightOffers.prediction.post();
+      expect(amadeus.client.post)
+        .toHaveBeenCalledWith('/v1/shopping/flight-offers/prediction', {});
     });
 
     it('.amadeus.shopping.hotelOffers.get', () => {
