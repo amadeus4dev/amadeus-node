16ddea85264f1cdc9205572c9f54b73569dbfeb4
Gustavo GIUNCO BERTOLDI <gg.bertoldi@hotmail.com>
2022-09-09 09:45:47 +0200

    Minor fix

--- src/amadeus/client/pagination.js
+++ src/amadeus/client/pagination.js
@@ -65,7 +65,7 @@ class Pagination {
   pageNumber(response, pageName) {
     try {
       return response.result['meta']['links'][pageName]
-        .split('page%5Boffset%5D=')[1]?.split('&')[0] || null;
+        .split('page%5Boffset%5D=')[1].split('&')[0];
     } catch (TypeError) {
       return null;
     }
